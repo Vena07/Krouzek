268639fe7cddde0082861eb6407967461dadede0
Vena07 <152257923+Vena07@users.noreply.github.com>
2024-09-26 08:27:05 +0200

    Add files via upload

---
+++ componets/Header.svelte
@@ -0,0 +1,34 @@
+<script>
+
+</script>
+
+<header>
+    <nav>
+        <a class = "Home" href="/">Home</a>
+        <a class="About" href="/about">About</a>
+        <a class="Svelte" href="/">Svelte</a>
+    </nav>
+</header>
+
+<style>
+    header{
+        height: 60px;
+        background-color: gray;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-radius: 10px;
+    }
+
+    .Home, .About, .Svelte{
+        margin: 50px;
+        color: white;
+        font-size: 25px;
+        border: solid 3px  rgb(100, 99, 99);
+        padding: 5px;
+        background-color: rgb(100, 99, 99);
+        text-decoration: none;
+        font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+        border-radius: 10px;
+    }
+</style>
